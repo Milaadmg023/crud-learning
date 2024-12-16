937d61240344fff7eaa2dca24e58810ef0212e8d
Milaadmg023 <bagerim962@gmail.com>
2024-12-16 11:49:19 +0330

    add: creating product schema

---
+++ models/products.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    qunaity: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", ProductSchema);
+
+module.exports = Product;
